802554ec64315538797f17efa011f9c5a7474ca2
Isabella <isabellamabuor@gmail.com>
2023-04-22 18:17:42 +0300

    Added the Contant functionality with Emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,7 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -18,51 +19,51 @@ const Contact = () => {
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
-    // const { target } = e;
-    // const { name, value } = target;
+    const { target } = e;
+    const { name, value } = target;
 
-    // setForm({
-    //   ...form,
-    //   [name]: value,
-    // });
+    setForm({
+      ...form,
+      [name]: value,
+    });
   };
 
   const handleSubmit = (e) => {
-    // e.preventDefault();
-    // setLoading(true);
+    e.preventDefault();
+    setLoading(true);
+  
+    emailjs
+      .send( 
+        'service_nvx88jc',
+        'template_auvwi4n',       
+        {
+          from_name: form.name,
+          to_name: "Isabella Abuor",
+          from_email: form.email,
+          to_email: "isabellamabuor@gmail.com",
+          message: form.message,
+        },
+        'l8PHVD8qqlAm6JKHO'
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
 
-    // emailjs
-    //   .send(
-    //     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-    //     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-    //     {
-    //       from_name: form.name,
-    //       to_name: "Isabella Abuor",
-    //       from_email: form.email,
-    //       to_email: "isabellamabuor@gmail.com",
-    //       message: form.message,
-    //     },
-    //     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    //   )
-    //   .then(
-    //     () => {
-    //       setLoading(false);
-    //       alert("Thank you. I will get back to you as soon as possible.");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
 
-    //       setForm({
-    //         name: "",
-    //         email: "",
-    //         message: "",
-    //       });
-    //     },
-    //     (error) => {
-    //       setLoading(false);
-    //       console.error(error);
-
-    //       alert("Ahh, something went wrong. Please try again.");
-    //     }
-    //   );
-  };
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      )
+  }
 
   return (
     <div
