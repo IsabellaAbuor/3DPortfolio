413ec4d81b39bd77a7c212a7ca6238691b706f38
Isabella <isabellamabuor@gmail.com>
2024-03-05 13:22:18 +0300

    changes 5

--- src/components/index.js
+++ src/components/index.js
@@ -2,21 +2,21 @@ import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas'
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
-import Tech from './Tech';
-import Experience from './Experience';
-import Works from './Works';
-import Feedbacks from './Feedbacks';
+// import Tech from './Tech';
+// import Experience from './Experience';
+// import Works from './Works';
+// import Feedbacks from './Feedbacks';
 import Contact from './Contact';
-import { backend } from '../assets';
+// import { backend } from '../assets';
 
 export {
   Hero,
   Navbar,
   About,
-  Tech,
-  Experience,
-  Works,
-  Feedbacks,
+  // Tech,
+  // Experience,
+  // Works,
+  // Feedbacks,
   Contact,
   EarthCanvas, 
   BallCanvas, 
